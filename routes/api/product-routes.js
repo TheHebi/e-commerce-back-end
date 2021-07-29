50e99cc0fa8f023158ba3404ac92bf60e49ce61b
TheHebi <nturcotte8@live.com>
2021-07-29 09:57:43 -0700

    full CRUD functionality for all routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,10 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try{
     const product = await Product.findAll({
-      include: [{model:Category, attributes: [`id`, `category_name`]}, {model:Tag, attributes: {exclude: [`createdAt`, `updatedAt`]}}],
+      include: [{model:Category, attributes: [`id`, `category_name`]}, 
+      {model:Tag, attributes: {exclude: [`createdAt`, `updatedAt`]},
+      through:{attributes: {exclude: [`createdAt`,`updatedAt`]}}}],
+      
       attributes: {exclude: [`createdAt`,`updatedAt`]}
     })
     res.status(200).json(product)
@@ -25,7 +28,10 @@ router.get('/:id', async (req, res) => {
   try{
     const product = await Product.findOne({
       where: {id:req.params.id},
-      include: [{model:Category, attributes: [`id`, `category_name`]}, {model:Tag, attributes: {exclude: [`createdAt`, `updatedAt`]}}],
+      include: [{model:Category, attributes: [`id`, `category_name`]}, 
+      {model:Tag, attributes: {exclude: [`createdAt`, `updatedAt`]},
+      through:{attributes: {exclude: [`createdAt`,`updatedAt`]}}}],
+      
       attributes: {exclude: [`createdAt`,`updatedAt`]}
     })
     res.status(200).json(product)
@@ -108,8 +114,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const delProduct = await Product.destroy({
+      where:{id:req.params.id}
+    });
+    res.status(200).json(delProduct)
+  }catch(err){
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
